360f69561344fad968389c3f7c2bb058c92901d3
Rahul Arora <rahularora@Rahuls-MacBook-Pro.local>
2022-11-27 19:43:36 +0530

    Sample example 1

---
+++ src/main/java/com.example/App.java
@@ -0,0 +1,18 @@
+package com.example;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+public class App {
+
+    private static final Logger logger = LogManager.getLogger(App.class);
+
+    public static void main(String[]args) {
+
+        try {
+            int result = 1000/0;
+        } catch (Exception e) {
+            logger.error("Error occurred!");
+        }
+    }
+}
